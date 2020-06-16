dd13137ec18caa283f8770e394bece6ea12a5a6f
Aiden <66179677+Aiden76005588@users.noreply.github.com>
2020-06-16 20:43:07 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,10 +1,17 @@
-print(2**3)
-print(5 % 3)  # 나머지 구하기 2
-print(10 % 3)
-print(5//3)  # 목
-print(10 > 3)
-print(4 >= 7)
-print(5 <= 5)
+print((2 + 3) * 4)
+number = 2 + 3 * 4
+print(number)
+number = number + 2
+print(number)
+number += 2
+print(number)
+number *= 2
+print(number)
+number /= 2
+print(number)
+number -= 2
+print(number)
 
-print(3 == 3)  # True 같다
-print(4 == 2)
+
+number %= 2
+print(number)
